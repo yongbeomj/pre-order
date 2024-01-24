48693c78879a24d7c7cb5e0fed050b5465ecb289
Yongbeom Jang <alex.ybjang@gmail.com>
2024-01-24 13:45:30 +0900

    Initial commit

---
+++ src/main/java/com/shop/preorder/PreOrderApplication.java
@@ -0,0 +1,13 @@
+package com.shop.preorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/preorder/PreOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.preorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
