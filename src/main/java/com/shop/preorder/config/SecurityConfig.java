e268e5aa744eaa4eed54d6b329e970bfda6a2bad
Yongbeom Jang <alex.ybjang@gmail.com>
2024-01-26 12:54:13 +0900

    chore: SecurityConfig 추가
    - PasswordEncoder 추가 : 비밀번호 암호화
    - SecurityFilterChain 추가 : spring security 권한 설정

---
+++ src/main/java/com/shop/preorder/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.shop.preorder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests.anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
