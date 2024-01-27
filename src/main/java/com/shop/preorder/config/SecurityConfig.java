e56975815374312ef3498659f41bb15a625f2915
Yongbeom Jang <alex.ybjang@gmail.com>
2024-01-28 00:45:02 +0900

    chore: jwt 환경 세팅
    - dependency 추가
    - config 추가
    - application.yml 사용 세팅값 추가

--- src/main/java/com/shop/preorder/config/SecurityConfig.java
+++ src/main/java/com/shop/preorder/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.shop.preorder.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -20,9 +24,18 @@ public class SecurityConfig {
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests.anyRequest().permitAll()
+
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/users/join/**").permitAll()
+                        .requestMatchers("/api/users/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // 세션 미사용
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
+
         return http.build();
     }
+
 }
