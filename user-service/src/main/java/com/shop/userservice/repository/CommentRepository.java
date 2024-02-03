fdcd59d8ffe055600d54def8fedbd41e8276d1db
Yongbeom Jang <alex.ybjang@gmail.com>
2024-02-03 16:25:11 +0900

    remove: user-service 관련 로직 외 파일 제거

--- user-service/src/main/java/com/shop/userservice/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.userservice.repository;
-
-import com.shop.userservice.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- user-service/src/main/java/com/shop/userservice/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.userservice.repository;
-
-import com.shop.userservice.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
