6652950a8d73a1968c864e2e97dc502d85ed9028
Yongbeom Jang <alex.ybjang@gmail.com>
2024-02-03 16:42:49 +0900

    feat: activity-service global 예외처리 추가

--- activity-service/src/main/java/com/shop/activityservice/exception/GlobalExceptionHandler.java
+++ activity-service/src/main/java/com/shop/activityservice/exception/GlobalExceptionHandler.java
@@ -12,17 +12,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<?> errorHandler(BaseException e) {
+    public ResponseEntity<?> globalException(BaseException e) {
         log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(ResponseDto.error(ErrorResponse.of(e.getErrorCode())));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> databaseErrorHandler(IllegalArgumentException e) {
+    public ResponseEntity<?> globalException(IllegalArgumentException e) {
         log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(ErrorCode.DATABASE_ERROR.getHttpStatus())
                 .body(ResponseDto.error(ErrorResponse.of(ErrorCode.DATABASE_ERROR)));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> globalException(RuntimeException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(ErrorCode.SYSTEM_ERROR.getHttpStatus())
+                .body(ResponseDto.error(ErrorResponse.of(ErrorCode.SYSTEM_ERROR)));
     }
 
 }
