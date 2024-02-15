2f58067fbd1243f073f6fe7f6274a891b7b8bc2a
Yongbeom Jang <alex.ybjang@gmail.com>
2024-02-15 18:07:31 +0900

    feat: order service 모듈 생성

---
+++ module-order-service/src/main/java/com/shop/orderservice/OrderServiceApplication.java
@@ -0,0 +1,16 @@
+package com.shop.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
