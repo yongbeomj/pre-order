82fbee7fffe7f6ef3812f8afb78da1991c2c4bf2
Yongbeom Jang <alex.ybjang@gmail.com>
2024-02-27 21:18:45 +0900

    remove: root project src 폴더 삭제

--- src/main/java/com/shop/preorder/domain/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.shop.preorder.domain;
-
-public enum UserRole {
-    ADMIN, USER
-}
--- src/main/java/com/shop/preorder/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.preorder.repository;
-
-import com.shop.preorder.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
